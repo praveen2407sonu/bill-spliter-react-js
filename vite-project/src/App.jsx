aac42f10375c86adbdd75dd788cbbf2cc55ade8e
praveen2407sonu <psnethrana@gmail.com>
2025-05-27 15:22:53 +0530

    updated file

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -11,6 +11,14 @@ function BillSplitter() {
   const [amountPerPerson, setAmountPerPerson] = useState();
 
   const calculateBill = () => {
+
+     if (!bill || !tipPercentage || !people) {
+    alert('Please fill all input fields .');
+    return;
+  }
+    
+
+
     const billValue = Number(bill);
     const peopleValue = Number(people);
     const tipValue = Number(tipPercentage);
@@ -26,32 +34,43 @@ function BillSplitter() {
   };
 
   return (
+   
+    <div className="container">
+  <div className="inputs">
+    <h2>Bill Splitter</h2>
     <div>
-      <h2>Bill Sipliter</h2>
-      <div>
-        <label>Bill Amount</label>
-        <input type="number" placeholder='Bill Amount' value={bill} 
-        onChange={(e) => setBill(e.target.value)} />
-      </div>
-      <div>
-        <label>Tip Percentage</label>
-        <input type="number" placeholder='Tip amount percent' value={tipPercentage} 
-        onChange={(e) => setTipPercentage(e.target.value)} />
-      </div>
-
-      <div>
-        <label>Number of People</label>
-        <input type="number" placeholder='Number of pepole' value={people} 
-        onChange={(e) => setPeople(e.target.value)} />
-      </div>
-      
-      <button onClick={calculateBill}>Calculate Bill</button>
-      <div>
-        <p>Tip Amount {tipAmount}</p>
-        <p>Total Bill {totalBill}</p>
-        <p>Amount Per Person {amountPerPerson}</p>
-      </div>
+      <label>Bill Amount</label><br /><br />
+      <input type="number" placeholder='Bill Amount' value={bill} 
+      onChange={(e) => setBill(e.target.value)} />
     </div>
+    <div>
+      <label>Tip Percentage</label><br /><br />
+
+     <ul className='unlist' >
+                {[5,10,15,25,50,75].map((value)=>(
+                    
+                    <li key={value} className='list' onClick={(e)=>setTipPercentage(value)}>{value}%</li>
+                ))}
+            </ul>
+
+      <input type="number" placeholder='Tip amount percent' value={tipPercentage} 
+      onChange={(e) => setTipPercentage(e.target.value)} />
+    </div>
+    <div>
+      <label>Number of People</label><br /><br />
+      <input type="number" placeholder='Number of people' value={people} 
+      onChange={(e) => setPeople(e.target.value)} />
+    </div>
+    <button className='btn' onClick={calculateBill}>Calculate Bill</button>
+  </div>
+
+  <div className="results">
+    <p><strong>Tip Amount:</strong> {tipAmount}</p>
+    <p><strong>Total Bill:</strong> {totalBill}</p>
+    <p><strong>Amount Per Person:</strong> {amountPerPerson}</p>
+  </div>
+</div>
+
   );
 }
 
